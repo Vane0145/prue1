b28f4be720e998bf52885995892c198a7c825f2b
mi_vanessa <mi_canokarla034@gmail.com>
2023-06-20 09:09:23 -0600

    homework

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+
+  it('prueba 1', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('prueba 2', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('prueba 3', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+})
