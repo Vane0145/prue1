e8c3c64e069053253acb56b211f92809849a14b3
mi_vanessa <mi_canokarla034@gmail.com>
2023-06-24 06:34:49 -0600

    Homework

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,8 @@
 describe('template spec', () => {
 
-it("the h1 contains the correct text", () => {
-    cy.visit("https://example.cypress.io")
-  })
+//it("the h1 contains the correct text", () => {
+    //cy.visit("https://example.cypress.io")
+ // })
 
  //it('xpath test', () => {
  // cy.visit('https://example.cypress.io')
@@ -10,9 +10,14 @@ it("the h1 contains the correct text", () => {
  // cy.xpath('//h1').should('have.length', 1);
 //});
 
-it("the h1 contains the correct text", () => {
-  cy.visit("http://localhost:3000")
-  cy.get("h1")
+//it("the h1 contains the correct text", () => {
+  //cy.visit("http://localhost:3000")
+  //cy.get("h1")
+  
+//})
+  it.only("the features on the homepage are correct", () => {
+   cy.visit("http://localhost:3000")
+   cy.get("dt").eq(1).contains("25+ Lessons")
 })
 
 
