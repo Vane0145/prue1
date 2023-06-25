40d4596dd8dde828dec3d1a9c64869c8fa15650b
mi_vanessa <mi_canokarla034@gmail.com>
2023-06-25 05:09:03 -0600

    Homework

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,12 +4,20 @@ describe('First Homework', () => {
 
   })
 
-it.only("the features on the homepage are correct", () => {
+it("the features on the homepage are correct", () => {
   cy.get("dt").eq(1).contains("25+ Lessons")
 
 })
-it.only("the features on the homepage are correct", () => {
+it("the features on the homepage are correct", () => {
   cy.get("dt").eq(2).should("contain", "Free and Open Source")
+})
+it("Suscribe for updates", () => {
+  cy.get('[placeholder="Subscribe for Updates"]').type("Vanessa")
+  cy.get('[value="Subscribe"]').click()
+  cy.get('[data-test="success-message"]')
+
+
+
 })
 
 
