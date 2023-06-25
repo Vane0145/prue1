4d1e80ea9c9994629cac2f876ca285151c4d0b2f
mi_vanessa <mi_canokarla034@gmail.com>
2023-06-25 03:22:59 -0600

    FirtHomework

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,23 +1,15 @@
-describe('template spec', () => {
+describe('First Homework', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
 
-//it("the h1 contains the correct text", () => {
-    //cy.visit("https://example.cypress.io")
- // })
+  })
 
- //it('xpath test', () => {
- // cy.visit('https://example.cypress.io')
- // cy.xpath('//h1').contains("Kitchen Sink")
- // cy.xpath('//h1').should('have.length', 1);
-//});
+it.only("the features on the homepage are correct", () => {
+  cy.get("dt").eq(1).contains("25+ Lessons")
 
-//it("the h1 contains the correct text", () => {
-  //cy.visit("http://localhost:3000")
-  //cy.get("h1")
-  
-//})
-  it.only("the features on the homepage are correct", () => {
-   cy.visit("http://localhost:3000")
-   cy.get("dt").eq(1).contains("25+ Lessons")
+})
+it.only("the features on the homepage are correct", () => {
+  cy.get("dt").eq(2).should("contain", "Free and Open Source")
 })
 
 
